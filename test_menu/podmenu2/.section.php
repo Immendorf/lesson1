45db26079249410c27d3178fd3013a2976eb46ec
Immendorf <ideem93@gmail.com>
2021-03-28 19:29:37 +0800

    Мой второй коммит

---
+++ test_menu/podmenu2/.section.php
@@ -0,0 +1,3 @@
+<?php
+$sSectionName = "Подменю 2";
+?>
---
+++ test_menu/podmenu2/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Подменю 2");
+echo "Получили доступ к подменю 2";
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
